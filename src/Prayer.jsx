551ffbcd8254297f33394cb1ae7b69c3820459bf
RedaRasmy <red7mer@gmail.com>
2024-09-09 10:43:43 +0100

    update

--- src/Prayer.jsx
+++ src/Prayer.jsx
@@ -5,7 +5,7 @@ import CardMedia from '@mui/material/CardMedia';
 
 export default function MediaCard({imgUrl,title,time}) {
   return (
-    <Card sx={{ maxWidth: 1000 , minWidth: 230}} style={{margin:'5px',height:300,direction:'rtl'}}>
+    <Card sx={{ maxWidth: 1000 , minWidth: 210}} style={{margin:'5px',height:300,direction:'rtl'}}>
       <CardMedia
         sx={{ height: 140 }}
         image={imgUrl}
